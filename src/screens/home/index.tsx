ac72a615809a8f1593b7486c8a358f80e4b30cf0
Mateus Macedo <105501644+Mateusfdsm1@users.noreply.github.com>
2023-11-16 12:33:25 -0300

    Creation of all boxes and bottom form

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
+
+function handleParticipantAdd() {
+    console.log("Teste clique");
+}
   return(
     <View style= {styles.container}>
       <Text style= {styles.eventName}
@@ -9,6 +13,20 @@ export default function Home() {
       </Text>  
       <Text style= {styles.eventDate}
       >Sexta, 4 de Novembro de 2022.</Text>
+       
+      <View style= {styles.form}>
+      <TextInput 
+         style = {styles.input} 
+         placeholder="Nome do Participante"
+         placeholderTextColor= "#6B6B6B"
+      />
+
+        <TouchableOpacity style= {styles.button} onPress= {handleParticipantAdd}>
+            <Text style= {styles.buttonText}>
+                + 
+            </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
     },
   
     eventName: {
-      color: '#FDFCFE',
+      color: '#FFF',
       fontSize: 24,
       fontWeight: 'bold',
       marginTop: 48
@@ -18,5 +18,39 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: '#6B6B6B',
       fontSize: 16
-    }  
+    },  
+ 
+     input: {
+        flex: 1,
+        height: 55,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+        marginRight: 12
+     },
+     
+     buttonText: {
+        color: '#FFF',
+        fontSize: 24
+     },
+
+     button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31CF67',
+        justifyContent: 'center',
+        alignItems: 'center'
+     },
+
+     form: {
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42
+     }
+
+
   });
